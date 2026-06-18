049ee907022e9c3036ae9a6fe1d0b9a9e644c1b1
taiyme <53635909+taiyme@users.noreply.github.com>
2026-06-18 22:56:00 +0900

    fix(ssh): programs.ssh.{matchBlocks -> settings}

--- modules/flakes/security/ssh.nix
+++ modules/flakes/security/ssh.nix
@@ -1,42 +1,49 @@
 {
   flake.modules.homeManager.security =
-    { ... }:
+    { lib, ... }:
     {
       programs.ssh = {
         enable = true;
 
         enableDefaultConfig = false;
 
-        matchBlocks = {
-          "*" = {
-            addKeysToAgent = "yes";
-            serverAliveInterval = 60;
-            serverAliveCountMax = 5;
-            extraOptions.UseKeychain = "yes";
-            extraOptions.TCPKeepAlive = "yes";
-          };
+        # NOTE: bool は yes/no に置換してくれるみたい
+        settings =
+          let
+            matchBlocks = {
+              "Host github.com" = {
+                HostName = "github.com";
+                User = "git";
+                IdentityFile = "~/.ssh/keys/github.pub";
+                IdentitiesOnly = true;
+              };
 
-          "github.com" = {
-            hostname = "github.com";
-            user = "git";
-            identityFile = "~/.ssh/keys/github.pub";
-            identitiesOnly = true;
-          };
+              "Host misskey" = {
+                HostName = "ec2-35-72-183-158.ap-northeast-1.compute.amazonaws.com";
+                User = "taiy";
+                IdentityFile = "~/.ssh/keys/misskey.pub";
+                IdentitiesOnly = true;
+              };
 
-          "misskey" = {
-            hostname = "ec2-35-72-183-158.ap-northeast-1.compute.amazonaws.com";
-            user = "taiy";
-            identityFile = "~/.ssh/keys/misskey.pub";
-            identitiesOnly = true;
+              "Host vaultwarden" = {
+                HostName = "ec2-54-250-244-70.ap-northeast-1.compute.amazonaws.com";
+                User = "taiy";
+                IdentityFile = "~/.ssh/keys/vaultwarden.pub";
+                IdentitiesOnly = true;
+              };
+            };
+          in
+          matchBlocks
+          // {
+            # NOTE: これ↓↓が無いと、一番先頭に配置されてしまうので困る
+            "Host *" = lib.hm.dag.entryAfter (builtins.attrNames matchBlocks) {
+              AddKeysToAgent = true;
+              ServerAliveInterval = 60;
+              ServerAliveCountMax = 5;
+              UseKeychain = true;
+              TCPKeepAlive = true;
+            };
           };
-
-          "vaultwarden" = {
-            hostname = "ec2-54-250-244-70.ap-northeast-1.compute.amazonaws.com";
-            user = "taiy";
-            identityFile = "~/.ssh/keys/vaultwarden.pub";
-            identitiesOnly = true;
-          };
-        };
       };
     };
 }
